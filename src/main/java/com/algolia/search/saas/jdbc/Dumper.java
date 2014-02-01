09aa723e493b24cca49fe64000c1ac284aed79cc
Xavier Grand <grand.xavier@gmail.com>
2014-02-01 13:04:10 -0800

    Redesigned architecture

--- src/main/java/com/algolia/search/saas/jdbc/Dumper.java
+++ src/main/java/com/algolia/search/saas/jdbc/Dumper.java
@@ -1,11 +1,7 @@
 package com.algolia.search.saas.jdbc;
 
 import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
-
 import org.apache.commons.cli.ParseException;
 import org.json.JSONException;
 import org.json.simple.JSONObject;
@@ -17,60 +13,21 @@ public class Dumper extends Worker {
     public Dumper(JSONObject configuration) throws SQLException, ParseException, JSONException {
         super(configuration);
         
-        String batchSize = (String) this.configuration.get(Connector.CONF_BATCH_SIZE);
-        this.batchSize = batchSize == null ? 1000 : Integer.parseInt(batchSize);
+        
         this.query = (String) configuration.get(Connector.CONF_QUERY);
         assert (query != null);
-        this.idField = (String) configuration.get(Connector.CONF_UNIQUE_ID_FIELD);
-        assert (idField != null);
         this.stmt = database.prepareStatement(query, ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY);
-        stmt.setFetchSize(Integer.MIN_VALUE);
-        if (stmt instanceof com.mysql.jdbc.Statement) {
-            ((com.mysql.jdbc.Statement) stmt).enableStreamingResults();
+        this.stmt.setFetchSize(Integer.MIN_VALUE);
+        if (this.stmt instanceof com.mysql.jdbc.Statement) {
+            ((com.mysql.jdbc.Statement) this.stmt).enableStreamingResults();
         }
-        //TODO Activate on the other jdbc
     }
 
     @Override
     public void run() throws SQLException, AlgoliaException, JSONException {   
-    	ResultSet rs = stmt.executeQuery();
-        try {
-            ResultSetMetaData rsmd = rs.getMetaData();
-            int columns = rsmd.getColumnCount();
-            List<org.json.JSONObject> actions = new ArrayList<org.json.JSONObject>();
-            while (rs.next()) {
-                org.json.JSONObject obj = new org.json.JSONObject();
-                for (int i = 1; i < columns + 1; i++) {
-                    try {
-                    	if (rsmd.getColumnName(i).equals(idField)) {
-                    		obj.put("objectID", rs.getObject(i));
-                    	} else {
-                    		obj.put(rsmd.getColumnName(i), rs.getObject(i));
-                    	}
-                    } catch (JSONException e) {
-                        throw new Error(e);
-                    }
-                }
-                org.json.JSONObject action = new org.json.JSONObject();
-                action.put("action", "addObject");
-                action.put("body",obj);
-                actions.add(action);
-                if (actions.size() >= batchSize) {
-                	actions = addSetting(actions, "0"); //TODO
-                    //this.index.batch(actions); TODO
-                    actions.clear();
-                }
-            }
-            if (!actions.isEmpty()) {
-                this.index.addObjects(actions);
-            }
-        } finally {
-            rs.close();
-        }
+    	iterateOnQuery(stmt);
     }
 
     private final java.sql.PreparedStatement stmt;
     private final String query;
-    private final String idField;
-    private final int batchSize;
 }
--- src/main/java/com/algolia/search/saas/jdbc/Updater.java
+++ src/main/java/com/algolia/search/saas/jdbc/Updater.java
@@ -1,18 +1,39 @@
 package com.algolia.search.saas.jdbc;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import org.json.JSONException;
 import org.json.simple.JSONObject;
 
+import com.algolia.search.saas.AlgoliaException;
+
 public class Updater extends Worker {
     public Updater(JSONObject configuration) throws SQLException, org.apache.commons.cli.ParseException, JSONException {
         super(configuration);
-        this.currentTime = 0;
+        this.lastUpdatedAt = "0";
+        this.query = (String) configuration.get(Connector.CONF_QUERY);
+        assert (query != null);
     }
     
     @Override
-    public void run() {
+    public void run() throws SQLException, JSONException, AlgoliaException {
+    	try {
+			org.json.JSONObject settings = index.getSettings();
+			lastUpdatedAt = settings.getJSONObject("userDate").getString("lastUpdatedAt");
+		} catch (Exception e) {
+			//No index or uninitialized settings 
+		}
+    	String sql = query.replaceAll("_\\$", lastUpdatedAt);
+    	final java.sql.PreparedStatement stmt = database.prepareStatement(sql, ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY);
+        stmt.setFetchSize(Integer.MIN_VALUE);
+        if (stmt instanceof com.mysql.jdbc.Statement) {
+            ((com.mysql.jdbc.Statement) stmt).enableStreamingResults();
+        }
+        //TODO Activate on the other jdbc
+    	
+    	
+    	iterateOnQuery(stmt);
     }
 
 //    public boolean fetchDataBase() throws SQLException, AlgoliaException, JSONException {
@@ -34,5 +55,6 @@ public class Updater extends Worker {
 //        return true;
 //    }
     
-    private long currentTime;
+    private String lastUpdatedAt;
+    private final String query;
 }
