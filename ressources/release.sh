c9ada65c819307e0ba1ac22fad226022853ab3df
Xavier Grand <grand.xavier@gmail.com>
2014-09-10 11:12:32 +0200

    Fixed #3: Check if uu{en|de}code is installed

--- ressources/release.sh
+++ ressources/release.sh
@@ -4,14 +4,18 @@ if [ "$#" != "2" ]; then
   echo "Usage : $0 path/to/jar-with-dependencies.jar jdbc-connector.sh"
   exit 1
 fi
+command -v uuencode >/dev/null 2>&1 || { echo >&2 "uuencode is required, please install it first."; exit 1; }
 
 cat > $2 <<- EOF
 #! /bin/sh
 
 BIN=\`mktemp /tmp/algolia.XXXX\`
-uudecode -o \$BIN \$0
 
 command -v java >/dev/null 2>&1 || { echo >&2 "Java is required, please install it first."; exit 1; }
+command -v uudecode >/dev/null 2>&1 || { echo >&2 "uudecode is required, please install it first."; exit 1; }
+
+uudecode -o \$BIN \$0
+
 
 java -jar \$BIN "\$@"
 RETURN_VALUE=`echo $?`
